2906ee5cd2b4901ce765826b59c40d6937251253
Rajeev-Ranjanc <itrajeevranjan@gmail.com>
2024-03-29 19:52:44 +0530

    leetocde potd

--- SubArrayAtLeastKElements.java
+++ SubArrayAtLeastKElements.java
@@ -1,6 +1,5 @@
 //https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/?envType=daily-question&envId=2024-03-29
 
-import java.util.HashMap;
 import java.util.Map;
 
 /*
@@ -14,6 +13,38 @@ public class SubArrayAtLeastKElements {
     }
 
     public static long countSubarrays(int[] nums, int k) {
-       
+
+        int i = 0;
+        int j = 0;
+        long n = nums.length;
+        long maxelement = Long.MIN_VALUE;
+        long count = 0;
+
+        //finding maximum elements
+        for (long num : nums) {
+           maxelement  = Math.max(maxelement , num);
+        }
+
+        ///now apply sliding window
+        long countmaximum = 0;
+        while (j < nums.length) {
+            if (nums[j] == maxelement) {
+                countmaximum++;
+            }
+
+            while (countmaximum >= k) {
+
+                count += n - j;
+                if (nums[i] == maxelement) {
+                    countmaximum--;
+                }
+
+                i++;
+            }
+
+            j++;
+        }
+
+        return count;
     }
 }
