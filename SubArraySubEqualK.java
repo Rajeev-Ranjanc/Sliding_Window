c3c9ee203a0bd1109d75909f83fdd2739de8c2ae
Rajeev-Ranjanc <itrajeevranjan@gmail.com>
2024-06-04 08:51:48 +0530

    folder displacement

---
+++ SubArraySubEqualK.java
@@ -0,0 +1,16 @@
+//getting error leetcode 560
+public class SubArraySubEqualK {
+    public static void main(String[] args) {
+        int[] nums = {-1, -1, 1};
+        int k = 0;
+        System.out.println(subarraySum(nums, k));
+    }
+
+    public static int subarraySum(int[] nums, int k) {
+        //optimal one is here using sliding window
+//        this problem is going to solve by the prefix sum, and I'm applying the sliding window that's why I'm
+//        getting error
+
+        return 0;
+    }
+}
